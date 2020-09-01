e4aa30586890943cd314dc722651fbe97a4b8e4e
Jacob Short <jrshort89@gmail.com>
2020-09-01 02:05:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def join_nested_strings(src)
   while count1 < src.length do
   count2 = 0
     while count2 < src[count1].length do
-      if src[count1][count2].length
+      if src[count1][count2]
         str + src[count1][count2]
       end
       count2 += 1
